1e2104bbaf7bf5acdde3abab5a599a6d3f3042ca
Rudi Schlatte <rudi@constantly.at>
2025-12-12 11:15:30 +0100

    Read scenario from csv file
    This is for the non-NebulOuS publication and testing case

--- optimiser-digital-twin/src/main/java/eu/nebulouscloud/optimiser/twin/DeploymentImporter.java
+++ optimiser-digital-twin/src/main/java/eu/nebulouscloud/optimiser/twin/DeploymentImporter.java
@@ -18,11 +18,15 @@ import org.slf4j.MDC;
 import com.fasterxml.jackson.core.JsonPointer;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.MappingIterator;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.fasterxml.jackson.dataformat.csv.CsvMapper;
+import com.fasterxml.jackson.dataformat.csv.CsvSchema;
 
 import groovyjarjarpicocli.CommandLine.ParentCommand;
 import lombok.extern.slf4j.Slf4j;
+import picocli.CommandLine.ArgGroup;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
@@ -38,11 +42,22 @@ public class DeploymentImporter implements Callable<Integer> {
     @Parameters(index = "0", description = "The database file to be created")
     private Path dbFile;
 
-    @Parameters(index = "1", description = "A file containing the app creation message")
-    private Path appCreationMessageFile;
+    @ArgGroup(exclusive=true, multiplicity="1")
+    Input input;
+
+    static class Input {
+        @Option(names = { "-m", "--app-creation-message"}, description = "A file containing the (Nebulous) app creation message")
+        private Optional<Path> appCreationMessageFile;
+
+        @Option(names = { "-d", "--deployment-file" }, description = "A CSV files containing a deployment scenario")
+        private Optional<Path> deploymentScenarioFile;
+    }
 
     @Option(names = { "-s", "--solution" }, description = "A file containing a solver message with machine configurations")
     private Optional<Path> solverSolutionFile;
+
+    // @JsonPropertyOrder({ "Component", "Replicas", "Cores", "Memory"})
+    private record CsvEntry(String Component, int Replicas, int Cores, int Memory) {};
 
     @Override
     public Integer call() {
@@ -50,30 +65,86 @@ public class DeploymentImporter implements Callable<Integer> {
         NebulousApp app = null;
         JsonNode appMessage = null;
         JsonNode solverSolution = null;
-        try {
-            appMessage = mapper.readTree(Files.readString(appCreationMessageFile, StandardCharsets.UTF_8));
-        } catch (IOException e) {
-            log.error("Could not read app creation message file: {}", appCreationMessageFile);
-            return 1;
-        }
-        try {
-            app = NebulousApp.fromAppMessage(appMessage);
-        } catch (JsonProcessingException e) {
-            log.error("Could not parse app creation message", e);
-            return 1;
-        }
-        try {
-            if (solverSolutionFile.isPresent()) {
-                solverSolution = mapper.readTree(Files.readString(solverSolutionFile.get(),
-                    StandardCharsets.UTF_8));
+        if (input.appCreationMessageFile.isPresent()) {
+            try {
+                appMessage = mapper.readTree(Files.readString(input.appCreationMessageFile.get(), StandardCharsets.UTF_8));
+            } catch (IOException e) {
+                log.error("Could not read app creation message file: {}", input.appCreationMessageFile.get());
+                return 1;
             }
-        } catch (IOException e) {
-            log.error("Could not read solver solution file: {}", solverSolutionFile);
+            try {
+                app = NebulousApp.fromAppMessage(appMessage);
+            } catch (JsonProcessingException e) {
+                log.error("Could not parse app creation message", e);
+                return 1;
+            }
+            try {
+                if (solverSolutionFile.isPresent()) {
+                    solverSolution = mapper.readTree(Files.readString(solverSolutionFile.get(),
+                        StandardCharsets.UTF_8));
+                }
+            } catch (IOException e) {
+                log.error("Could not read solver solution file: {}", solverSolutionFile);
+            }
+            boolean success = saveSolverSolution(dbFile, app, solverSolution);
+            return success ? 0 : 1;
+        } else if (input.deploymentScenarioFile.isPresent()) {
+            if (solverSolutionFile.isPresent()) {
+                log.warn("Solver solutions only apply to app creation messages; ignoring {} during import of CSV deployment scenario",
+                    solverSolutionFile.get());
+            }
+            try {
+                String csv = Files.readString(input.deploymentScenarioFile.get());
+                boolean success = saveCsvScenario(dbFile, csv);
+                return success ? 0 : 1;
+            } catch (IOException e) {
+                log.error("Could not create database file", e);
+                return 1;
+            }
+        } else {
+            // should be impossible
+            log.error("Need either app creation message or cvs scenario; aborting");
+            return 1;
         }
-        boolean success = saveSolverSolution(dbFile, app, solverSolution);
-        return success ? 0 : 1;
     }
 
+    /**
+     * Create a database with the nodes specified by the given CSV data.
+     *
+     * @param dbName the name of the database to be created
+     * @param csvString the CSV data as a string
+     * @return true if the database creation was successful, false otherwise.
+     */
+    public static boolean saveCsvScenario(Path dbName, String csvString) {
+        CsvMapper csvMapper = new CsvMapper();
+        CsvSchema schema = CsvSchema.emptySchema()
+            .withHeader();
+
+        try (Connection connection = initDatabase(dbName);
+             PreparedStatement insert = connection.prepareStatement("""
+                    INSERT INTO scenario (component, cpu, memory, replicas)
+                    VALUES (?, ?, ?, ?)
+                    """))
+        {
+            MappingIterator<CsvEntry> it = csvMapper
+                .readerFor(CsvEntry.class)
+                .with(schema)
+                .readValues(csvString);
+            for (CsvEntry entry : it.readAll()) {
+                insert.setString(1, entry.Component);
+                insert.setInt(2, entry.Cores);
+                insert.setInt(3, entry.Memory);
+                insert.setInt(4, entry.Replicas);
+                insert.addBatch();
+            }
+            int[] results = insert.executeBatch();
+            log.info("Created {} entries in database {}", results.length, dbName);
+        } catch (SQLException | IOException e) {
+            log.error("Could not create database", e);
+            return false;
+        }
+        return true;
+    }
 
     /** Location of the deploy flag in the solver solution message */
     private static final JsonPointer deployPath = JsonPointer.compile("/DeploySolution");
@@ -82,8 +153,8 @@ public class DeploymentImporter implements Callable<Integer> {
 
 
     /**
-     * Create a database with the nodes specified by the app's kubevela, as
-     * modified by the given solution if not null.
+     * Create a database with the nodes specified by the app's kubevela,
+     * optionally modified by the given solution.
      *
      * @param dbName the name of the database to be created
      * @param app the NebulousApp
@@ -106,23 +177,14 @@ public class DeploymentImporter implements Callable<Integer> {
             effectiveKubevela = app.rewriteKubevelaWithSolution(variableValues);
         }
         Map<String, Integer> replicas = KubevelaAnalyzer.getNodeCount(effectiveKubevela);
-        try (Connection connection = DriverManager.getConnection("jdbc:sqlite:" + dbName);
-             Statement statement = connection.createStatement()) {
-            MDC.pushByKey("appId", app.getUuid());
 
-            statement.executeUpdate("DROP TABLE IF EXISTS scenario");
-            statement.executeUpdate("""
-                CREATE TABLE scenario (
-                  component STRING,
-                  cpu NUMBER,
-                  memory NUMBER,
-                  replicas NUMBER)
-                """);
-
-            try (PreparedStatement insert = connection.prepareStatement("""
+        try (Connection connection = initDatabase(dbName);
+             PreparedStatement insert = connection.prepareStatement("""
                     INSERT INTO scenario (component, cpu, memory, replicas)
                     VALUES (?, ?, ?, ?)
-                    """)) {
+                    """))
+        {
+            MDC.pushByKey("appId", app.getUuid());
                 for (JsonNode c : KubevelaAnalyzer.getNodeComponents(effectiveKubevela).values()) {
                     String componentName = c.at("/name").asText();
                     long cpu = KubevelaAnalyzer.getCpuRequirement(c, componentName);
@@ -136,7 +198,6 @@ public class DeploymentImporter implements Callable<Integer> {
                 }
                 int[] results = insert.executeBatch();
                 log.info("Created {} entries in database {}", results.length, dbName);
-            }
         } catch (SQLException e) {
             log.error("Could not create database", e);
             return false;
@@ -144,5 +205,28 @@ public class DeploymentImporter implements Callable<Integer> {
             MDC.popByKey("appId");
         }
         return true;
+    }
+
+    /**
+     * Initialize the database and table.  Note that the returned {@code
+     * Connection} object needs to be closed.
+     *
+     * @param dbName the database to be created
+     * @return the database connection
+     * @throws SQLException if creating the database or table failed
+     */
+    private static Connection initDatabase(Path dbName) throws SQLException {
+        Connection connection = DriverManager.getConnection("jdbc:sqlite:" + dbName);
+        Statement statement = connection.createStatement();
+        statement.executeUpdate("DROP TABLE IF EXISTS scenario");
+        statement.executeUpdate("""
+                CREATE TABLE scenario (
+                  component STRING,
+                  cpu NUMBER,
+                  memory NUMBER,
+                  replicas NUMBER)
+                """);
+        statement.close();
+        return connection;
     }
 }
