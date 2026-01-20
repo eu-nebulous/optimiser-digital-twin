ad3e44cb578483f5ff0d2ce51606dbe0df871c06
Rudi Schlatte <rudi@constantly.at>
2026-01-20 10:30:07 +0100

    In server mode, initialize with info from optimiser-controller
    - Modify the `serve` sub-command:
      - Add short CLI options
      - Add parameter for application id

--- optimiser-digital-twin/src/main/java/eu/nebulouscloud/optimiser/twin/ExnConnector.java
+++ optimiser-digital-twin/src/main/java/eu/nebulouscloud/optimiser/twin/ExnConnector.java
@@ -17,8 +17,10 @@ import eu.nebulouscloud.exn.Connector;
 import eu.nebulouscloud.exn.core.Consumer;
 import eu.nebulouscloud.exn.core.Context;
 import eu.nebulouscloud.exn.core.Handler;
+import eu.nebulouscloud.exn.core.Publisher;
 import eu.nebulouscloud.exn.handlers.ConnectorHandler;
 import eu.nebulouscloud.exn.settings.StaticExnConfig;
+import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 
 /**
@@ -47,29 +49,21 @@ public class ExnConnector {
             synchronized(this) {
                 while (context_ == null) {
                     try {
-	        	wait();
-	            } catch (final InterruptedException e) {
+                        wait();
+                    } catch (final InterruptedException e) {
                         log.error("Caught InterruptException while waiting for ActiveMQ connection Context; looping", e);
-	            }
+                    }
                 }
             }
         }
         return context_;
     }
 
-    /** A counter to create unique names for SyncedPublisher instances. */
-    private final AtomicInteger publisherNameCounter = new AtomicInteger(1);
-
     /** if non-null, signals after the connector is stopped */
     private CountDownLatch synchronizer = null;
 
     private static final ObjectMapper mapper = new ObjectMapper();
 
-    /** The topic where we listen for app creation messages. */
-    // Note that there is another, earlier app creation message sent via the
-    // channel `eu.nebulouscloud.ui.application.new`, but its format is not
-    // yet defined as of 2024-01-08.
-    public static final String app_creation_channel = "eu.nebulouscloud.ui.dsl.generic";
     /** The topic with an application's relevant performance indicators. */
     public static final String performance_indicators_channel =
         "eu.nebulouscloud.optimiser.utilityevaluator.performanceindicators";
@@ -78,6 +72,24 @@ public class ExnConnector {
     public static final String solver_solution_channel = "eu.nebulouscloud.optimiser.solver.solution";
     /** The per-app status channel, read by at least the UI and the solver. */
     public static final String app_status_channel = "eu.nebulouscloud.optimiser.controller.app_state";
+    /** The topic for receiving the app creation message after notifying the
+     * optimiser-controller we started */
+    public static final String twin_init_channel = "eu.nebulouscloud.optimiser.controller.twin_init";
+
+
+    /** The status channel for notifying the optimiser-controller that the
+     * digital twin started.  We send out an app's creation message on the
+     * channel named by {@link #ampl_message_channel} when getting the
+     * "started" message from a digital twin. */
+    public static final String twin_status_channel = "eu.nebulouscloud.optimiser.twin.state";
+
+    /**
+      * The Message producer for notifying the optimiser-controller that the digital twin started
+      */
+    @Getter
+    private final Publisher statusPublisher = new Publisher("controller_ampl", twin_status_channel, true, true);
+
+    private final ObjectMapper jsonMapper = new ObjectMapper();
 
     /**
      * Create a connection to ActiveMQ via the exn middleware, and set up the
@@ -87,27 +99,32 @@ public class ExnConnector {
      * @param port the port of the ActiveMQ server (usually 5672)
      * @param name the login name to use
      * @param password the login password to use
+     * @param appId the application id of the application we're twinning
      */
-    public ExnConnector(final String host, final int port, final String name, final String password) {
+    public ExnConnector(final String host, final int port, final String name, final String password, final String appId) {
+        MDC.put("appId", appId);
         conn = new Connector(
-            "optimiser_controller",
+            "optimiser_digital_twin",
             new ConnectorHandler() {
                 public void onReady(final Context context) {
                     ExnConnector.this.context_ = context;
                     synchronized(ExnConnector.this) {
                         ExnConnector.this.notifyAll();
                     }
-                    log.info("Optimiser-controller connected to ActiveMQ, got connection context {}", context);
+                    // Informing optimiser-controller that we're ready
+                    ObjectNode msg = jsonMapper.createObjectNode();
+                    msg.put("state", "started");
+                    getStatusPublisher().send(jsonMapper.convertValue(msg, Map.class), appId, true);
+                    log.info("Optimiser digital twin connected to ActiveMQ, got connection context {}", context);
                 }
             },
             List.of(
-                // Asynchronous topics for sending out controller status.
-                // Synchronous publishers are created dynamically, not added
-                // here.
-            ),
+                // Asynchronous topics for sending out messages.  Synchronous
+                // publishers are created dynamically, not added here.
+                statusPublisher),
             List.of(
-                new Consumer("ui_app_messages", app_creation_channel,
-                    new AppCreationMessageHandler(), true, true),
+                new Consumer("ui_app_messages", twin_init_channel,
+                    new InitMessageHandler(), true, true),
                 new Consumer("performance_indicator_messages", performance_indicators_channel,
                     new PerformanceIndicatorMessageHandler(), true, true),
                 new Consumer("solver_solution_messages", solver_solution_channel,
@@ -150,29 +167,36 @@ public class ExnConnector {
     // Message Handlers
 
     /**
-     * A message handler that processes app creation messages coming in via
-     * `eu.nebulouscloud.ui.dsl.generic`.  Such messages contain, among
-     * others, the KubeVela YAML definition and mapping from KubeVela
-     * locations to AMPL variables.
+     * A message handler that processes init messages.
+     *
+     * <p> An init message is sent by the optimiser-controller on {@code
+     * eu.nebulouscloud.optimiser.controller.twin_init} after receiving a
+     * "state: started" message on {@code eu.nebulouscloud.twin.state}.  The
+     * message has a {@code dsl} key with the app creation message, and a
+     * {@code solution} key with the synthetic solver message with the initial
+     * deployment.
      *
      * <p>Upon receiving a message, extract the list of components and the
-     * static part of the deployment scenario, then wait for the synthetic
-     * solver solution message sent by the optimiser-controller for the
-     * dynamic parts of the deployment scenario.
+     * static part of the deployment scenario from the app creation message,
+     * and the dynamic parts of the deployment scenario from the synthetic
+     * solver solution.
      */
-    public class AppCreationMessageHandler extends Handler {
+    public class InitMessageHandler extends Handler {
         @Override
         public void onMessage(final String key, final String address, final Map body, final Message message, final Context context) {
             try {
-                log.info("App creation message received");
-                final JsonNode appMessage = mapper.valueToTree(body);
-                final String appID = appMessage.at("/uuid").asText();
+                log.debug("Initialization message received");
+                final JsonNode initMessage = mapper.valueToTree(body);
+                final JsonNode dslMessage = initMessage.at("/dsl");
+                final JsonNode solverMessage = initMessage.at("/solution");
+                final String appID = dslMessage.at("/uuid").asText();
+                // TODO: check if appID equals our own
                 MDC.put("appId", appID);
-                Main.logFile("app-message-" + appID + ".json", appMessage.toPrettyString());
-                // TODO: initialize twin with list of components.  Don't
-                // calculate the initial deployment, we'll wait for the
-                // synthetic solver solution message from the
-                // optimiser-controller
+                log.info("Twin received initialization message for app id {}", appID);
+                Main.logFile("init-message-" + appID + ".json", initMessage.toPrettyString());
+                // TODO: initialize twin with list of components, and
+                // calculate the initial deployment.  Initialize twin and wait
+                // for log files to start calibrating.
             } catch (final RuntimeException e) {
                 log.error("Error while receiving app creation message", e);
             } finally {
@@ -226,8 +250,6 @@ public class ExnConnector {
     public class SolverSolutionMessageHandler extends Handler {
         @Override
         public void onMessage(final String key, final String address, final Map body, final Message message, final Context context) {
-            // We'll talk a lot with SAL etc, so we should maybe fire up a
-            // thread so as not to block here.
             try {
                 final ObjectNode json_body = mapper.convertValue(body, ObjectNode.class);
                 final String app_id = message.property("application").toString(); // should be string already, but don't want to cast
@@ -237,13 +259,11 @@ public class ExnConnector {
                 }
                 MDC.put("appId", app_id);
                 Main.logFile("solver-solution-" + app_id + ".json", json_body.toPrettyString());
-                // Three cases here:
-                // - The initial synthetic solution: initialize twin, start
-                //   calibrating
+                // Two cases here:
                 // - A real solution (deploy = true): reinitialize twin,
                 //   discard calibrated parameters, wait for deployment to
                 //   finish
-                // - A what-if solution (deploy = false): evaluate it
+                // - A what-if solution (deploy = false): simulate it
             } catch (final Exception e) {
                 log.error("Error while processing solver solutions message", e);
             } finally {
